7e79787624fdf064e19aa1bdf7d33233748e3a8a
aviloff <53415673+aviloff@users.noreply.github.com>
2022-08-04 23:31:33 +0200

    adding material page

---
+++ assets/loaders.js
@@ -0,0 +1,6 @@
+// https://stackoverflow.com/questions/17636528/how-do-i-load-an-html-page-in-a-div-using-javascript
+(
+function load_home() {
+     document.getElementById("social").innerHTML='<object type="text/html" data="/assets/snips/social.html" ></object>';
+}
+)
